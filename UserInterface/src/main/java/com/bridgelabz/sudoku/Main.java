a7969a0c192b4005c07d7d61b969f9bffea285a0
MohammedAtif-BL <98643253+MohammedAtif-BL@users.noreply.github.com>
2023-10-10 16:57:58 +0530

    Adding file

---
+++ UserInterface/src/main/java/com/bridgelabz/sudoku/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.sudoku;
+
+public class Main {
+    public static void main(String[] args) {
+        SudokuApplication.main(new String[]{});
+    }
+}
---
+++ UserInterface/src/main/java/com/bridgelabz/sudoku/SudokuApplication.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.sudoku;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class SudokuApplication  extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        //Get SudokuGame object for a new game
+//        uiImpl = new UserInterfaceImpl(primaryStage);
+//
+//        try {
+//            SudokuBuildLogic.build(uiImpl);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            throw e;
+//        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
